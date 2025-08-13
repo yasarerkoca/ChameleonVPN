4e36535d90c52190fb50717db72ea674aae8619c
yasarerkoca <125214336+yasarerkoca@users.noreply.github.com>
2025-08-14 02:06:36 +0300

    Delete web-user directory

--- web-user/src/__tests__/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('renders home page', () => {
-  render(<App />);
-  expect(screen.getByText(/home/i)).toBeInTheDocument();
-});
